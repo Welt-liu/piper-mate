0d3007a34b1e461672c30807d8cdad8d6d49cc22
Welt-liu <1994524450@qq.com>
2025-12-22 09:45:09 +0800

    Update README with installation guide and simplify Firefly config

--- Lerobot/lerobot_teleoperator_firefly/lerobot_teleoperator_firefly/config_starai_firefly.py
+++ Lerobot/lerobot_teleoperator_firefly/lerobot_teleoperator_firefly/config_starai_firefly.py
@@ -8,8 +8,7 @@ from lerobot.teleoperators.config import TeleoperatorConfig
 class FireflyConfig(TeleoperatorConfig):
     # Port to connect to the arm
     port: str
-    baudrate:int = 115200
-    use_degrees: bool = False
+    baudrate:int = 1000000
     joint_dir = [-1,1,1,-1,1,-1]
     angle_min = [-150,0,-170,-100,-70,-120,0]
     angle_max = [150,180,0,100,70,120,100]
